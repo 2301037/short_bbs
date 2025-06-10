9aaa1ef49c211d2090927146f51f63be5d6ca52a
Su-3214 <2301026@s.asojuku.ac.jp>
2025-06-10 12:35:30 +0900

    form変更

--- form.php
+++ form.php
@@ -7,14 +7,32 @@
 </head>
 <body>
     <h1>💬 一言掲示板</h1>
-    <form action="post.php" method="post">
+    <form action="" method="post">
         <p>名前：<input type="text" name="name" required></p>
         <p>コメント：<br>
         <textarea name="comment" rows="4" cols="40" required></textarea></p>
         <p><button type="submit">投稿する</button></p>
     </form>
+    <!-- ↓ PHP SQL CREATED ↓ -->
+    <?php
+    if($_SERVER["REQUEST_METHOD"] === "POST"){
+        $name = $_POST['name'] ?? '';
+        $comment = $_POST['comment'] ?? '';
+ 
+        $pdo = new PDO('mysql:host=mysql321.phy.lolipop.lan;
+                    dbname=LAA1553845-team1kadai1;charset=utf8',
+                    'LAA1553845',
+                    'Banana1234');
+    $sql = "INSERT INTO comment (name , comment) VALUES (:name,:comment)";
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(':name', $name);
+    $stmt->bindParam(':comment', $comment);
+    $stmt->execute();
+    }
+   
+                    ?>
+ 
     <p><a href="view.php">▶ 投稿一覧を見る</a></p>
 </body>
 </html>
-
-aaa
+ 
